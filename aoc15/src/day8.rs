00ff40ca47fc4e600a4652544cdde4030c4b4c5f
komer <hyluo.1999@qq.com>
2025-11-21 16:00:02 +0800

    finish day8

--- aoc15/src/day8.rs
+++ aoc15/src/day8.rs
@@ -5,13 +5,55 @@ use linkme::distributed_slice;
 
 const INPUT: &str = include_str!("../inputs/day8.txt");
 
+fn parse_line(line: &str) -> usize {
+    let mut count = 0;
+    let mut iter = line.as_bytes().iter();
+    while let Some(ch) = iter.next() {
+        match ch {
+            b'\\' => match iter.next().unwrap() {
+                b'\\' => count += 1,
+                b'"' => count += 1,
+                b'x' => {
+                    iter.next();
+                    iter.next();
+                    count += 1;
+                }
+                _ => panic!("invalid escape"),
+            },
+            b'"' => {}
+            _ => count += 1,
+        }
+    }
+
+    count
+}
+
 #[distributed_slice(PUZZLES)]
 pub fn puzzle0() -> String {
-    let _ = INPUT;
-    format_result!("template 0");
+    let total_chars: usize = INPUT.lines().map(|line| line.len()).sum();
+    let parsed_chars: usize = INPUT.lines().map(parse_line).sum();
+    format_result!(total_chars - parsed_chars);
+}
+
+fn encode_line(line: &str) -> String {
+    let mut res = String::new();
+    res.push('"');
+
+    for ch in line.as_bytes().iter() {
+        match ch {
+            b'\\' => res.push_str("\\\\"),
+            b'"' => res.push_str("\\\""),
+            _ => res.push(*ch as char),
+        }
+    }
+    res.push('"');
+
+    res
 }
 
 #[distributed_slice(PUZZLES)]
 pub fn puzzle1() -> String {
-    format_result!("template 1");
+    let total_chars: usize = INPUT.lines().map(|line| line.len()).sum();
+    let encoded_chars: usize = INPUT.lines().map(|line| encode_line(line).len()).sum();
+    format_result!(encoded_chars - total_chars);
 }
