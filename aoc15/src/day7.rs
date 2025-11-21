14f0ccc38cd331f52377d1f5352f1c4708e73bc2
komer <hyluo.1999@qq.com>
2025-11-21 15:16:37 +0800

    opt day7 with AI

--- aoc15/src/day7.rs
+++ aoc15/src/day7.rs
@@ -2,295 +2,260 @@ use crate::PUZZLES;
 use crate::format_result;
 
 use linkme::distributed_slice;
-
-use std::cell::RefCell;
-use std::collections::HashMap;
-use std::rc::Rc;
+use nom::{
+    IResult, Parser,
+    branch::alt,
+    bytes::complete::tag,
+    character::complete::{alpha1, line_ending, u16 as nom_u16},
+    combinator::{map, map_res},
+    multi::many1,
+    sequence::separated_pair,
+};
+use rustc_hash::FxHashMap;
 
 const INPUT: &str = include_str!("../inputs/day7.txt");
 
-#[derive(Eq, PartialEq, Clone, Copy)]
-enum Func {
-    Wire,
-    Not,
-    And,
-    Or,
-    Rshift,
-    Lshift,
-    Consts,
+type Wire = u16;
+
+#[derive(Debug, Clone, Copy)]
+enum Input {
+    Wire(Wire),
+    Signal(u16),
 }
 
-struct Component {
-    valid: bool,
-    output: u16,
-    inputs: Vec<String>,
-    func: Func,
+impl Input {
+    fn value(&self, signals: &[Option<u16>]) -> Option<u16> {
+        match self {
+            Input::Wire(wire) => signals[*wire as usize],
+            Input::Signal(signal) => Some(*signal),
+        }
+    }
 }
 
-impl Component {
-    fn new(func: Func) -> Self {
+#[derive(Debug, Clone, Copy)]
+enum Gate {
+    Set(Input),
+    And(Input, Input),
+    Or(Input, Input),
+    LShift(Input, u16),
+    RShift(Input, u16),
+    Not(Input),
+}
+
+impl Gate {
+    fn eval(&self, signals: &[Option<u16>]) -> Option<u16> {
+        match self {
+            Gate::Set(a) => a.value(signals),
+            Gate::And(a, b) => {
+                if let (Some(a), Some(b)) = (a.value(signals), b.value(signals)) {
+                    Some(a & b)
+                } else {
+                    None
+                }
+            }
+            Gate::Or(a, b) => {
+                if let (Some(a), Some(b)) = (a.value(signals), b.value(signals)) {
+                    Some(a | b)
+                } else {
+                    None
+                }
+            }
+            Gate::LShift(a, b) => a.value(signals).map(|a| a << *b),
+            Gate::RShift(a, b) => a.value(signals).map(|a| a >> *b),
+            Gate::Not(a) => a.value(signals).map(|a| !a),
+        }
+    }
+}
+
+enum ParsedInput<'a> {
+    Wire(&'a str),
+    Signal(u16),
+}
+
+enum ParsedGate<'a> {
+    Set(ParsedInput<'a>, &'a str),
+    And(ParsedInput<'a>, ParsedInput<'a>, &'a str),
+    Or(ParsedInput<'a>, ParsedInput<'a>, &'a str),
+    LShift(ParsedInput<'a>, u16, &'a str),
+    RShift(ParsedInput<'a>, u16, &'a str),
+    Not(ParsedInput<'a>, &'a str),
+}
+
+fn parse_input(i: &str) -> IResult<&str, ParsedInput<'_>> {
+    alt((
+        map(nom_u16, ParsedInput::Signal),
+        map(alpha1, ParsedInput::Wire),
+    ))
+    .parse(i)
+}
+
+fn parse_gate(i: &str) -> IResult<&str, ParsedGate<'_>> {
+    let parse_set = map(
+        separated_pair(parse_input, tag(" -> "), alpha1),
+        |(input, wire)| ParsedGate::Set(input, wire),
+    );
+    let parse_not = map(
+        separated_pair((tag("NOT "), parse_input), tag(" -> "), alpha1),
+        |((_, input), wire)| ParsedGate::Not(input, wire),
+    );
+    let parse_op = map(
+        separated_pair(
+            (
+                parse_input,
+                alt((
+                    map(tag(" AND "), |_| "AND"),
+                    map(tag(" OR "), |_| "OR"),
+                    map(tag(" LSHIFT "), |_| "LSHIFT"),
+                    map(tag(" RSHIFT "), |_| "RSHIFT"),
+                )),
+                parse_input,
+            ),
+            tag(" -> "),
+            alpha1,
+        ),
+        |((in1, op, in2), wire)| match op {
+            "AND" => ParsedGate::And(in1, in2, wire),
+            "OR" => ParsedGate::Or(in1, in2, wire),
+            "LSHIFT" => {
+                if let ParsedInput::Signal(val) = in2 {
+                    ParsedGate::LShift(in1, val, wire)
+                } else {
+                    panic!("LSHIFT requires a signal as the second argument");
+                }
+            }
+            "RSHIFT" => {
+                if let ParsedInput::Signal(val) = in2 {
+                    ParsedGate::RShift(in1, val, wire)
+                } else {
+                    panic!("RSHIFT requires a signal as the second argument");
+                }
+            }
+            _ => unreachable!(),
+        },
+    );
+
+    alt((parse_not, parse_op, parse_set)).parse(i)
+}
+
+fn parse_circuit(i: &str) -> (Vec<(Gate, Wire)>, FxHashMap<String, Wire>) {
+    let (_rem, parsed_gates) = many1(map_res((parse_gate, line_ending), |(g, _)| {
+        Ok::<_, &str>(g)
+    }))
+    .parse(i)
+    .unwrap();
+
+    let mut wires = FxHashMap::default();
+    let mut next_wire = 0;
+    let mut gates = vec![];
+
+    let get_wire = |s: &str, wires: &mut FxHashMap<String, Wire>, next_wire: &mut u16| {
+        *wires.entry(s.to_string()).or_insert_with(|| {
+            let ret = *next_wire;
+            *next_wire += 1;
+            ret
+        })
+    };
+
+    let to_input = |parsed: ParsedInput,
+                    wires: &mut FxHashMap<String, Wire>,
+                    next_wire: &mut u16| match parsed {
+        ParsedInput::Signal(s) => Input::Signal(s),
+        ParsedInput::Wire(w) => Input::Wire(get_wire(w, wires, next_wire)),
+    };
+
+    for parsed_gate in parsed_gates {
+        match parsed_gate {
+            ParsedGate::Set(input, wire) => {
+                let input = to_input(input, &mut wires, &mut next_wire);
+                let wire = get_wire(wire, &mut wires, &mut next_wire);
+                gates.push((Gate::Set(input), wire));
+            }
+            ParsedGate::And(in1, in2, wire) => {
+                let in1 = to_input(in1, &mut wires, &mut next_wire);
+                let in2 = to_input(in2, &mut wires, &mut next_wire);
+                let wire = get_wire(wire, &mut wires, &mut next_wire);
+                gates.push((Gate::And(in1, in2), wire));
+            }
+            ParsedGate::Or(in1, in2, wire) => {
+                let in1 = to_input(in1, &mut wires, &mut next_wire);
+                let in2 = to_input(in2, &mut wires, &mut next_wire);
+                let wire = get_wire(wire, &mut wires, &mut next_wire);
+                gates.push((Gate::Or(in1, in2), wire));
+            }
+            ParsedGate::LShift(input, val, wire) => {
+                let input = to_input(input, &mut wires, &mut next_wire);
+                let wire = get_wire(wire, &mut wires, &mut next_wire);
+                gates.push((Gate::LShift(input, val), wire));
+            }
+            ParsedGate::RShift(input, val, wire) => {
+                let input = to_input(input, &mut wires, &mut next_wire);
+                let wire = get_wire(wire, &mut wires, &mut next_wire);
+                gates.push((Gate::RShift(input, val), wire));
+            }
+            ParsedGate::Not(input, wire) => {
+                let input = to_input(input, &mut wires, &mut next_wire);
+                let wire = get_wire(wire, &mut wires, &mut next_wire);
+                gates.push((Gate::Not(input), wire));
+            }
+        }
+    }
+
+    (gates, wires)
+}
+
+struct Evaluator {
+    gates: Vec<(Gate, Wire)>,
+    signals: Vec<Option<u16>>,
+    num_wires: u16,
+}
+
+impl Evaluator {
+    fn new(gates: Vec<(Gate, Wire)>, num_wires: u16) -> Self {
         Self {
-            valid: false,
-            output: 0,
-            inputs: vec![],
-            func,
+            gates,
+            signals: vec![None; num_wires as usize],
+            num_wires,
         }
     }
 
-    fn new_constant(val: u16) -> Self {
-        Self {
-            valid: true,
-            output: val,
-            inputs: vec![],
-            func: Func::Consts,
-        }
-    }
-
-    fn add_input(&mut self, comp: String) {
-        self.inputs.push(comp);
-    }
-}
-
-type Context = Rc<HashMap<String, RefCell<Component>>>;
-
-impl Component {
-    fn get_output(&mut self, context: Context) -> u16 {
-        match self.func {
-            Func::Consts => self.output,
-            Func::Wire => {
-                if self.valid {
-                    self.output
-                } else {
-                    assert!(self.inputs.len() == 1);
-                    self.output = context
-                        .get(&self.inputs[0])
-                        .expect("not existed gate")
-                        .borrow_mut()
-                        .get_output(context.clone());
-                    self.valid = true;
-                    self.output
-                }
-            }
-            Func::Not => {
-                if self.valid {
-                    self.output
-                } else {
-                    assert!(self.inputs.len() == 1);
-                    self.output = !context
-                        .get(&self.inputs[0])
-                        .expect("not existed gate")
-                        .borrow_mut()
-                        .get_output(context.clone());
-                    self.valid = true;
-                    self.output
-                }
-            }
-            Func::And => {
-                if self.valid {
-                    self.output
-                } else {
-                    assert!(self.inputs.len() == 2);
-                    let input0 = context
-                        .get(&self.inputs[0])
-                        .expect("not existed gate")
-                        .borrow_mut()
-                        .get_output(context.clone());
-                    let input1 = context
-                        .get(&self.inputs[1])
-                        .expect("not existed gate")
-                        .borrow_mut()
-                        .get_output(context.clone());
-                    self.output = input0 & input1;
-                    self.valid = true;
-                    self.output
-                }
-            }
-            Func::Or => {
-                if self.valid {
-                    self.output
-                } else {
-                    assert!(self.inputs.len() == 2);
-                    let input0 = context
-                        .get(&self.inputs[0])
-                        .expect("not existed gate")
-                        .borrow_mut()
-                        .get_output(context.clone());
-                    let input1 = context
-                        .get(&self.inputs[1])
-                        .expect("not existed gate")
-                        .borrow_mut()
-                        .get_output(context.clone());
-                    self.output = input0 | input1;
-                    self.valid = true;
-                    self.output
-                }
-            }
-            Func::Lshift => {
-                if self.valid {
-                    self.output
-                } else {
-                    assert!(self.inputs.len() == 2);
-                    let input0 = context
-                        .get(&self.inputs[0])
-                        .expect("not existed gate")
-                        .borrow_mut()
-                        .get_output(context.clone());
-                    let input1 = context
-                        .get(&self.inputs[1])
-                        .expect("not existed gate")
-                        .borrow_mut()
-                        .get_output(context.clone());
-                    self.output = input0 << input1;
-                    self.valid = true;
-                    self.output
-                }
-            }
-            Func::Rshift => {
-                if self.valid {
-                    self.output
-                } else {
-                    assert!(self.inputs.len() == 2);
-                    let input0 = context
-                        .get(&self.inputs[0])
-                        .expect("not existed gate")
-                        .borrow_mut()
-                        .get_output(context.clone());
-                    let input1 = context
-                        .get(&self.inputs[1])
-                        .expect("not existed gate")
-                        .borrow_mut()
-                        .get_output(context.clone());
-                    self.output = input0 >> input1;
-                    self.valid = true;
-                    self.output
+    fn eval(&mut self, wire: Wire) -> u16 {
+        while self.signals[wire as usize].is_none() {
+            for (gate, wire) in &self.gates {
+                if self.signals[*wire as usize].is_none() {
+                    self.signals[*wire as usize] = gate.eval(&self.signals);
                 }
             }
         }
-    }
-}
-
-fn parse_line(line: &str, context: &mut HashMap<String, RefCell<Component>>) {
-    let segs: Vec<&str> = line.split("->").collect();
-    // input
-    let inputs: Vec<&str> = segs[0].split_whitespace().collect();
-    let output_name = segs[1].trim().to_string();
-    let comp_name = segs[0].trim().to_string();
-    match inputs.len() {
-        1 => {
-            // add input
-            let input = inputs[0].to_string();
-            if input.chars().next().unwrap().is_ascii_digit() {
-                let constant: u16 = input.parse().expect("Not valid constant");
-                context.insert(
-                    comp_name.clone(),
-                    RefCell::new(Component::new_constant(constant)),
-                );
-            }
-            // add output wire
-            let mut comp = Component::new(Func::Wire);
-            comp.add_input(comp_name.clone());
-            context
-                .entry(output_name)
-                .and_modify(|c| c.borrow_mut().add_input(comp_name))
-                .or_insert(RefCell::new(comp));
-        }
-        2 => {
-            // add input
-            assert!(inputs[0] == "NOT");
-            let input = inputs[1].to_string();
-            let mut comp = Component::new(Func::Not);
-            comp.add_input(input);
-            context.insert(comp_name.clone(), RefCell::new(comp));
-            // add output wire
-            let mut comp = Component::new(Func::Wire);
-            comp.add_input(comp_name.clone());
-            context
-                .entry(output_name)
-                .and_modify(|c| c.borrow_mut().add_input(comp_name))
-                .or_insert(RefCell::new(comp));
-        }
-        3 => {
-            // add input
-            let input0 = inputs[0].to_string();
-            let func = match inputs[1] {
-                "AND" => Func::And,
-                "OR" => Func::Or,
-                "LSHIFT" => Func::Lshift,
-                "RSHIFT" => Func::Rshift,
-                _ => panic!("Not valid func"),
-            };
-            let mut comp = Component::new(func);
-            let input1 = inputs[2].to_string();
-            if input1.chars().next().unwrap().is_ascii_digit() {
-                let constant: u16 = input1.parse().expect("Not valid constant");
-                context.insert(
-                    input1.clone(),
-                    RefCell::new(Component::new_constant(constant)),
-                );
-            }
-            comp.add_input(input0);
-            comp.add_input(input1);
-            context.insert(comp_name.clone(), RefCell::new(comp));
-            // add output wire
-            let mut comp = Component::new(Func::Wire);
-            comp.add_input(comp_name.clone());
-            context
-                .entry(output_name)
-                .and_modify(|c| c.borrow_mut().add_input(comp_name))
-                .or_insert(RefCell::new(comp));
-        }
-        _ => panic!("Not valid inputs"),
-    }
-}
-
-fn construct_whole_circuit() -> Context {
-    let mut context = HashMap::new();
-
-    for line in INPUT.lines() {
-        parse_line(line, &mut context);
+        self.signals[wire as usize].unwrap()
     }
 
-    Rc::new(context)
+    fn reset(&mut self) {
+        self.signals = vec![None; self.num_wires as usize];
+    }
 }
 
 #[distributed_slice(PUZZLES)]
 pub fn puzzle0() -> String {
-    let context = construct_whole_circuit();
+    let (gates, wires) = parse_circuit(INPUT);
+    let mut evaluator = Evaluator::new(gates, wires.len() as u16);
+    let a_wire = wires["a"];
 
-    let output = context
-        .get("a")
-        .expect("not found circuit a")
-        .borrow_mut()
-        .get_output(context.clone());
-
+    let output = evaluator.eval(a_wire);
     format_result!(output);
 }
 
 #[distributed_slice(PUZZLES)]
 pub fn puzzle1() -> String {
-    let context = construct_whole_circuit();
+    let (gates, wires) = parse_circuit(INPUT);
+    let mut evaluator = Evaluator::new(gates, wires.len() as u16);
+    let a_wire = wires["a"];
+    let b_wire = wires["b"];
 
-    let output_a = context
-        .get("a")
-        .expect("not found circuit a")
-        .borrow_mut()
-        .get_output(context.clone());
+    let a_val = evaluator.eval(a_wire);
 
-    // reset
-    for comp in context.values() {
-        comp.borrow_mut().valid = false;
-    }
-    {
-        let mut comp_b = context.get("b").expect("not found circuit a").borrow_mut();
-        comp_b.valid = true;
-        comp_b.output = output_a;
-    }
+    evaluator.reset();
 
-    let output_a = context
-        .get("a")
-        .expect("not found circuit a")
-        .borrow_mut()
-        .get_output(context.clone());
-
-    format_result!(output_a);
+    evaluator.signals[b_wire as usize] = Some(a_val);
+    let output = evaluator.eval(a_wire);
+    format_result!(output);
 }
