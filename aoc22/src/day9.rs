15b4956581945e23d18e8c084d6dba140a595f00
komer <hyluo.1999@qq.com>
2025-11-27 13:38:30 +0800

    opt 22,9

--- aoc22/src/day9.rs
+++ aoc22/src/day9.rs
@@ -5,101 +5,50 @@ use linkme::distributed_slice;
 
 const INPUT: &str = include_str!("../inputs/day9.txt");
 
-enum Direction {
-    Up,
-    Down,
-    Left,
-    Right,
-}
+fn solve(input: &str, rope_length: usize) -> String {
+    let mut knots = vec![(0, 0); rope_length];
+    let mut visited = HashSet::from([(0, 0)]);
 
-struct Move {
-    direction: Direction,
-    num: i32,
-}
+    for line in input.lines() {
+        let (dir, amount) = line.split_once(' ').unwrap();
+        let amount: i32 = amount.parse().unwrap();
 
-fn parse_cmds(i: &str) -> Vec<Move> {
-    i.lines()
-        .map(|line| {
-            let mut ll = line.split_whitespace();
-            let dir = ll.next().unwrap();
-            let direction = match dir {
-                "L" => Direction::Left,
-                "R" => Direction::Right,
-                "U" => Direction::Up,
-                "D" => Direction::Down,
-                _ => panic!("Unknown direction: {}", dir),
-            };
+        for _ in 0..amount {
+            // Move the head knot
+            match dir {
+                "U" => knots[0].1 += 1,
+                "D" => knots[0].1 -= 1,
+                "L" => knots[0].0 -= 1,
+                "R" => knots[0].0 += 1,
+                _ => unreachable!(),
+            }
 
-            let num = ll.next().unwrap().parse::<i32>().unwrap();
-            Move { direction, num }
-        })
-        .collect()
-}
+            // Move the subsequent knots
+            for i in 1..rope_length {
+                let head = knots[i - 1];
+                let tail = &mut knots[i];
 
-#[derive(Clone, Copy)]
-struct Coord {
-    x: i32,
-    y: i32,
-}
-impl Coord {
-    fn move_by(&mut self, mv: &Direction) {
-        match mv {
-            Direction::Up => self.y += 1,
-            Direction::Down => self.y -= 1,
-            Direction::Left => self.x -= 1,
-            Direction::Right => self.x += 1,
+                let dx: i32 = head.0 - tail.0;
+                let dy: i32 = head.1 - tail.1;
+
+                if dx.abs() > 1 || dy.abs() > 1 {
+                    tail.0 += dx.signum();
+                    tail.1 += dy.signum();
+                }
+            }
+            // Record the position of the last knot
+            visited.insert(*knots.last().unwrap());
         }
     }
-    fn follow(&mut self, head: &Coord) {
-        if (self.x - head.x).abs() <= 1 && (self.y - head.y).abs() <= 1 {
-            return;
-        }
-        if self.y < head.y {
-            self.y += 1;
-        } else if self.y > head.y {
-            self.y -= 1;
-        }
-        if self.x < head.x {
-            self.x += 1;
-        } else if self.x > head.x {
-            self.x -= 1;
-        }
-    }
+    format_result!(visited.len())
 }
 
 #[distributed_slice(PUZZLES)]
 pub fn puzzle0() -> String {
-    let moves = parse_cmds(INPUT);
-    let mut head = Coord { x: 0, y: 0 };
-    let mut tail = Coord { x: 0, y: 0 };
-    let mut tail_records = HashSet::new();
-    for cmd in moves {
-        for _ in 0..cmd.num {
-            head.move_by(&cmd.direction);
-            tail.follow(&head);
-            println!("tail: {} {}", tail.x, tail.y);
-            tail_records.insert((tail.x, tail.y));
-        }
-    }
-    let ret = tail_records.len();
-    format_result!(ret)
+    solve(INPUT, 2)
 }
 
 #[distributed_slice(PUZZLES)]
 pub fn puzzle1() -> String {
-    let moves = parse_cmds(INPUT);
-    let mut rope = [Coord { x: 0, y: 0 }; 10];
-    let mut tail_records = HashSet::new();
-    for cmd in moves {
-        for _ in 0..cmd.num {
-            rope[0].move_by(&cmd.direction);
-            for i in 1..rope.len() {
-                let tmp = rope[i - 1];
-                rope[i].follow(&tmp);
-            }
-            tail_records.insert((rope[9].x, rope[9].y));
-        }
-    }
-    let ret = tail_records.len();
-    format_result!(ret)
+    solve(INPUT, 10)
 }
